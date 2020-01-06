8af23b9c9161ccf85bc578f36de7b8e28acec2cb
Gensuke Toshiro <toshigem@gmail.com>
2020-01-06 16:11:44 +0900

    Add example submission

---
+++ 01-rotation/predict.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
+
+if __name__ == '__main__':
+    # define command-line arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path',
+                        type=str)
+    parser.add_argument('--image_dir',
+                        type=str)
+    parser.add_argument('-o', '--output',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout)
+    args = parser.parse_args()
+
+    # load trained weights
+    model = load_model(args.model_path)
+
+    image_paths = [
+        os.path.join(args.image_dir, filename)
+        for filename in os.listdir(args.image_dir)
+        if os.path.splitext(filename)[-1].lower() in ['.jpg', '.jpeg', '.png']
+    ]
+
+    data_gen = (img_to_array(load_img(path)) for path in image_paths)
+
+    preds = model.predict_generator(data_gen)
+
+    writer = csv.writer(args.output)
+    for i, pred in enumerate(preds):
+        filename = os.path.basename(image_paths[i])
+        label = np.argmax(pred)
+        writer.writerow([filename, label * 90])
