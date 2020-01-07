69ca743bc1362f1f4a060ca041706fbcb103cbbe
Gensuke Toshiro <toshigem@gmail.com>
2020-01-07 13:51:30 +0900

    Create PR comment with evaluated score

--- .circleci/scripts/evaluate_rotation.py
+++ .circleci/scripts/evaluate_rotation.py
@@ -4,6 +4,18 @@ import argparse
 import numpy as np
 from keras import metrics, backend as K
 
+import common
+
+class INFO:
+    name = 'Document Image Rotation Correction'
+
+def compute_accuracy(y_pred=[], y_true=[]):
+    y_pred, y_true = np.array(y_pred, dtype=int), \
+        np.array(y_true, dtype=int)
+
+    acc = metrics.accuracy(y_pred, y_true)
+    return K.eval(K.sum(acc)) / y_true.shape[0]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('pred_path', type=str)
@@ -19,8 +31,9 @@ if __name__ == '__main__':
         true_dict = {name: label for name, label in csv.reader(true_f)}
         files = true_dict.keys()
 
-        y_pred, y_true = np.array([pred_dict.get(name) for name in files], dtype=int), \
-            np.array([true_dict.get(name) for name in files], dtype=int)
+        score = compute_accuracy(y_pred=[pred_dict.get(name) for name in files],
+                                 y_true=[true_dict.get(name) for name in files])
 
-        acc = metrics.accuracy(y_pred, y_true)
-        print(K.eval(K.sum(acc)) / y_true.shape[0])
+        common.comment_result(INFO, {'Accuracy': score})
+
+        print(score)
